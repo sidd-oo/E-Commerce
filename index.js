ce4d3ee73243bac2c66e8adea9d1a1b2283c931e
sidd_oo <sahoosiddharth3@gmail.com>
2021-05-03 18:13:28 +0530

    CRUD functionality added

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const mongoose = require('mongoose');
 const path = require("path")
 const seedDB = require("./seed")
 const productRoutes = require('./routes/product');
+const methodOverride = require("method-override");
 
 mongoose.connect('mongodb://localhost:27017/ecommerceDB', {
-    useNewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
 }).then(()=>{
     console.log("DB Connected");
 }).catch((err)=>{
     console.log("Error occurecting")
     console.log(err);
 })
-
+  
 // seedDB();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.urlencoded({encoded:true}))
+app.use(methodOverride('_method'))
 
 app.use(productRoutes);
 
--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,48 @@ const express = require('express')
 const router = express.Router();
 const Product = require('../models/product')
 
+//Display all the products
 router.get('/products',async(req,res)=>{
     const products = await Product.find({});
     res.render('products/index',{products});
 })
+
+//Get the form for the product
+router.get('/products/new',(req,res)=>{
+    res.render('products/new')
+})
+
+//Create a new product
+router.post('/products',async(req, res)=>{
+    await Product.create(req.body.product);
+    res.redirect('/products');
+})
+
+//Show a particular product
+router.get('/products/:id',async(req, res)=>{
+    const product = await Product.findById(req.params.id);
+    res.render('products/show', {product});
+})
+
+//Editing the particular product
+router.get('/products/:id/edit',async(req, res)=>{
+    const product = await Product.findById(req.params.id);
+    res.render('products/edit', {product});
+})
+
+//Updating a particular product
+router.patch('/products/:id', async(req, res)=>{
+    await Product.findByIdAndUpdate(req.params.id, req.body.product)
+    res.redirect(`/products/${req.params.id}`)
+})
+
+//Deleting a particular product
+router.delete('/products/:id',async(req, res)=>{
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect('/products');
+})
+
+
+
 
 module.exports = router;
